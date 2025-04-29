a0308a1e682a31a722d40327efae1078cefcca6c
shin <lt00104@gmail.com>
2025-04-29 14:55:07 +0900

    add send_resident_info() function

--- main_server/network/packet/client_packet.py
+++ main_server/network/packet/client_packet.py
@@ -21,7 +21,7 @@ class ClientPacket:
             packet.write_int(len(result))
             for row in result:
                 packet.write_string(row[0])
-                packet.write_string(row[1].strftime('%y-%m-%d'))
+                packet.write_string(row[1].strftime('%Y-%m-%d'))
 
         return packet.get_packet()
     
@@ -30,5 +30,18 @@ class ClientPacket:
 
         packet.write_opcode(Opcode.SEND_RESIDENT_INFO.value)
         packet.write_status(result)
+
+        return packet.get_packet()
+    
+    def send_resident_info(status, result):
+        packet = PacketBuilder()
+
+        packet.write_opcode(Opcode.REQUEST_RESIDENT_INFO.value)
+        packet.write_status(status)
+        if status == 0x00:
+            packet.write_string(result[0])
+            packet.write_char(result[1])
+            packet.write_string(result[2].strftime('%Y-%m-%d'))
+            packet.write_bytes(result[3])   # Images
 
         return packet.get_packet()
