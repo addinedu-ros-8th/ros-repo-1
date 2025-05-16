8ace0e5446c98ab4c1358dd33f417b964733674e
shin <lt00104@gmail.com>
2025-05-16 09:55:16 +0900

    add quaternion position data and update status in database

--- nuri_server/nuri_system/nuri_system/robot/robot_state.py
+++ nuri_server/nuri_system/nuri_system/robot/robot_state.py
@@ -1,21 +1,40 @@
 import time
+from rclpy.node import Node
+
+from nuri_system.database.datbase_connection import NuriDatabase
 
 class RobotState():
     def __init__(self, id):
         self.id = id
         self.battery = -1
-        self.status = "대기중"
+        self.status = "대기"
         self.last_update = 0.0
         self.online = False
-        self.x = 0
-        self.y = 0
+        self.x = -1
+        self.y = -1
+
+        self.q_x = 0
+        self.q_y = 0
+        self.q_z = 0
+        self.q_w = 0
 
     def update(self, status=None, battery=None):
         if status is not None:
             self.update_status(status)
+
+            conn = NuriDatabase.get_instance()
+            try:
+                query = "UPDATE robot SET status_id = (SELECT name FROM robot_status WHERE type = %s) WHERE id = %s"
+                conn.execute_query(query, (status, self.id))
+            except:
+                conn.rollback()
+
         if battery is not None:
             self.update_battery(battery)
-        self.online = True
+
+        if not self.online:
+            self.online = True
+            
         self.last_update = time.time()
 
     def update_status(self, status):
@@ -24,9 +43,13 @@ class RobotState():
     def update_battery(self, battery):
         self.battery = battery
 
-    def update_location(self, x, y):
-        self.x = x
-        self.y = y
+    def update_location(self, position, orientation):
+        self.x = position.x
+        self.y = position.y
+        self.q_x = orientation.x
+        self.q_y = orientation.y
+        self.q_z = orientation.z
+        self.q_w = orientation.w
 
     def get(self):
         return {
