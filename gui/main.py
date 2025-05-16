2502dccf26c101884b04b48fa201f290111763c1
shin <lt00104@gmail.com>
2025-05-16 09:37:26 +0900

    error handling

--- gui/main.py
+++ gui/main.py
@@ -116,6 +116,9 @@ class MainWindow(QMainWindow):
     def checkPassword(self):
         password = widgets.lineEdit_2.text()
         if password == "1234":
+            if len(self.ui.map.map_info) == 0:
+                QMessageBox.warning(self, "에러", "아직 로봇의 준비가 완료되지 않았습니다.\n잠시후 다시 시도해주세요.")
+                return
             UIFunctions.setComponentEnabled(self, True)
             widgets.lineEdit_2.setText("")
             # widgets.stackedWidget.setCurrentWidget(widgets.home)
@@ -165,6 +168,8 @@ class MainWindow(QMainWindow):
             btn.setStyleSheet(UIFunctions.selectMenu(btn.styleSheet())) # SELECT MENU
 
             self.socket.sendData(Packet.request_patrol_schedule())
+            self.socket.sendData(Packet.request_walk_schedule())
+            self.socket.sendData(Packet.request_resident_name_list())
 
 
     # RESIZE EVENTS
@@ -180,7 +185,11 @@ class MainWindow(QMainWindow):
         self.dragPos = event.globalPosition().toPoint()
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    # app.setWindowIcon(QIcon("icon.ico"))
-    window = MainWindow()
-    sys.exit(app.exec())
+    try:
+        app = QApplication(sys.argv)
+        # app.setWindowIcon(QIcon("icon.ico"))
+        window = MainWindow()
+    except:
+        QMessageBox.critical(None, "에러", "에러가 발생했습니다. 관리자에게 문의 바랍니다.")
+    finally:
+        sys.exit(app.exec())
